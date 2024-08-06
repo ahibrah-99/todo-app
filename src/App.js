76f513d87cfc35be7b66fc5148279ba6ce46888b
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-06 08:11:11 +0200

    Updated App.js with proper headers and module imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// useState => returns current state and a function to change it (use it to keep track of changes dynamically)
+// https://www.youtube.com/watch?v=O6P86uwfdR0&list=PLZlA0Gpn_vH8EtggFGERCwMY5u5hOjf-h
+
+// useEffect => used for saving and loading by (fetching data, local storage, etc)
+// https://www.youtube.com/watch?v=0ZJgIjIuY7U
+
+import localforage from 'localforage';  // For saving data locally 'Local Forage must be installed $npm install localforage'
+
+// For styling
 import './App.css';
 
 function App() {
